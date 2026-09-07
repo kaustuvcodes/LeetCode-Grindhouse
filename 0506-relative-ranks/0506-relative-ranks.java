431edec678d7d3cba8feb4f868abd1d7f70f3e96
Kaustuv Singh <kaustuvsingh23@gmail.com>
2026-09-07 22:50:13 +0530

    Time: 11 ms (54.96%), Space: 47.5 MB (72.61%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+        }
+        Arrays.sort(indices, (a, b) -> Integer.compare(score[b], score[a]));
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                result[indices[i]] = "Gold Medal";
+            } else if (i == 1) {
+                result[indices[i]] = "Silver Medal";
+            } else if (i == 2) {
+                result[indices[i]] = "Bronze Medal";
+            } else {
+                result[indices[i]] = String.valueOf(i + 1);
+            }
+        }
+        return result;
+    }
+}
